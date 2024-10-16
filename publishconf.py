654351fa3925bdfee01c158f1b073d275c557321
RichardNixon <nicola.schiavo@studenti.unipd.it>
2024-10-16 11:06:25 +0200

    remove atom feeds

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ from pelicanconf import *
 SITEURL = 'https://maresciallonino.github.io'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
